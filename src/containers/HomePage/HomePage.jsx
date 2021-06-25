dcd1e21544a69a15236b180d38b1e08121eafc59
Boyang Gao <boyang.gao@yahoo.com>
2021-06-25 19:14:19 +0800

    home page sylished

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,40 +1,11 @@
 import React from "react";
 import "./HomePage.scss";
+import Directory from "../../components/Directory/Directory";
+
 const HomePage = (props) => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HAT</h1>
-            <span className="subtitle">SHOP NOW!</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW!</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW!</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW!</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">SHOP NOW!</span>
-          </div>
-        </div>
-      </div>
+      <Directory/>
     </div>
   )
 }
