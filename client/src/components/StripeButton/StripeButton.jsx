695df9d0aea2bcdf1f5a6eac4594d4f963d706cb
Boyang Gao <boyang.gao@yahoo.com>
2021-07-11 22:59:42 +0800

    integrated backend payment server

--- client/src/components/StripeButton/StripeButton.jsx
+++ client/src/components/StripeButton/StripeButton.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios'
 
-const onToken = () => {
-  alert("Payment Success");
-}
+
 const stripeButton = ({price}) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51J9sHJDaaWwYnUcjI3DFSPNMgEHgddMFRQkmQLYLsYduWDo7tPucbtMOC1b1Tk9Z8ZMGhW7khjFvXgTk2hLabdVG00YoWUANRD'
+  const onToken = async (token) => {
+    try {
+      const res = await fetch('payment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          amount: priceForStripe,
+          token: token
+        })
+      });
+      alert('Payment Success')
+    } catch (error) {
+      console.log(JSON.parse(error));
+      alert('There was an issue about your payment, please check your credit card number');
+    }
+  }
+
   return (
     <StripeCheckout label = 'Pay Now'
                     name = "Boyang's Clothing Store"
